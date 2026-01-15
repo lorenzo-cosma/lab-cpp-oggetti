6a81912a45657a59f3e522f28c188ac42be439f8
lorenzo-cosma <ldcosma.scuola@gmail.com>
2026-01-15 08:16:45 +0100

    completed 1_studente_basic.cpp

--- 3-make/1_studente_basic.cpp
+++ 3-make/1_studente_basic.cpp
@@ -21,8 +21,18 @@ int main()
 {
     /*********************** Creazione di oggetti ******************************/
     // TODO Creare un nuovo studente "Mario" con eta 17 e media 7
+    Studente M;
+    M.nome="Mario";
+    M.eta=17;
+    M.media=7;
+    M.stampaStudente();
     // TODO Creare un nuovo studente "Maria"  (questa volta con un puntatore) con eta 18 e media 8
-
+     Studente A;
+     Studente *P=&A;
+    P->nome="Maria";
+    P->eta=18;
+    P->media=8;
+    P->stampaStudente();
 
     /************************** Visualizzazione ********************************/
     // TODO Visualizzare il nome e la media sia di Mario che Maria
